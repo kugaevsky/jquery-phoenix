10f287f4aaefcace7663029e77cc963c68924938
Vladimir Bolshakov <vovanbo@gmail.com>
2014-05-24 12:24:48 +0400

    Added package.json and gulpfile.js for build with gulp.

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var gulp = require('gulp'),
+plugins = require('gulp-load-plugins')();
+
+gulp.task('coffee', function () {
+	gulp.src('src/*.coffee')
+	.pipe(plugins.coffee().on('error', plugins.util.log))
+	.pipe(gulp.dest('./'))
+	.pipe(plugins.eslint({
+		globals: {
+			'jQuery': true
+		},
+		env: {
+			browser: true
+		}
+	}))
+	.pipe(plugins.eslint.format())
+	.pipe(plugins.size());
+});
+
+gulp.task('minify', function() {
+	gulp.src('jquery.phoenix.js')
+	.pipe(plugins.uglify())
+	.pipe(plugins.rename(function (path) {
+		if(path.extname === '.js') {
+			path.basename += '.min';
+		}
+	}))
+	.pipe(gulp.dest('./'))
+	.pipe(plugins.size());
+});
+
+gulp.task('build', ['coffee', 'minify']);
+
+gulp.task('watch', function () {
+	gulp.watch('src/*.coffee', ['coffee']);
+	gulp.watch('jquery.phoenix.js', ['minify']);
+});
